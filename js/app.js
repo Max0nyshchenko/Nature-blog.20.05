bbd24483f2ee53b5b9a36e9ed0a0afd9225adbd5
Max0nyshchenko <max.0nyshenk03@gmail.com>
2020-05-21 18:00:17 +0300

    menu display on burger click

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 // VARS
-const mainLBImg = document.querySelector('.interesting-fact-img');
-const video = document.querySelector('.showcase-video');
-const footer = document.querySelector('footer');
-const mainLB = document.querySelector('.main-left');
-const mainRB = document.querySelector('.main-right');
-const intFact = document.querySelector('.interesting-fact-text');
-const showcaseCont = document.querySelector('.showcase-container');
-const menuWrapper = document.querySelector('.menu-wrapper');
-const nav = document.querySelector('nav');
+const mainLBImg = document.querySelector(".interesting-fact-img");
+const video = document.querySelector(".showcase-video");
+const footer = document.querySelector("footer");
+const mainLB = document.querySelector(".main-left");
+const mainRB = document.querySelector(".main-right");
+const intFact = document.querySelector(".interesting-fact-text");
+const showcaseCont = document.querySelector(".showcase-container");
+const menuWrapper = document.querySelector(".menu-wrapper");
+const nav = document.querySelector("nav");
+const burger = document.querySelector(".burger");
 
 // ANIMATION PART
 let scroller =
@@ -23,7 +24,7 @@ function isElementInViewport(el) {
 }
 
 function looper(arr, cl, rem) {
-  if (typeof arr === 'array') {
+  if (typeof arr === "array") {
     arr.forEach((el) => el.classList.add(rem));
     arr.forEach((element) => {
       if (isElementInViewport(element)) {
@@ -46,49 +47,34 @@ function looper(arr, cl, rem) {
 }
 
 // Menu Part
-
-// function menuResp() {
-//   let clicked = 0;
-//   let burger = document.createElement('div');
-//   for (let i = 0; i < 3; i++) {
-//     let newLine = document.createElement('div');
-//     newLine.classList.add(`line${i + 1}`);
-//     burger.appendChild(newLine);
-//   }
-//   burger.classList.add('burger');
-//   nav.appendChild(burger);
-//   burger.style.top =
-//     nav.clientHeight / 2 -
-//     document.querySelector('.burger').clientHeight / 2 +
-//     'px';
-
-//   burger.addEventListener('click', () => {
-//     if (clicked) {
-//       menuWrapper.style.transition = 'display 1s';
-//       menuWrapper.style.display = 'none';
-//       clicked = 0;
-//     } else {
-//       menuWrapper.style.display = 'block';
-//       clicked = 1;
-//     }
-//   });
-// }
+let clicked = 0;
+function menuResp() {
+  burger.addEventListener("click", () => {
+    if (clicked == 0) {
+      menuWrapper.style.display = "grid";
+      clicked++;
+    } else if (clicked == 1) {
+      menuWrapper.style.display = "none";
+      clicked = 0;
+    }
+  });
+}
 
 // DOMCONTENT LOADED
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // Showcase video style
-  video.style.height = '100vh';
-  video.style.width = '100vw';
-  video.style.objectFit = 'cover';
+  video.style.height = "100vh";
+  video.style.width = "100vw";
+  video.style.objectFit = "cover";
   video.play();
   video.autoplay = true;
 
   //Animation
-  looper(mainLBImg, 'showImg', 'hideImg');
-  looper(mainLB, 'showMainLB', 'hideMainLB');
-  looper(intFact, 'showMainLB', 'hideMainLB');
-  looper(mainRB, 'showMainRB', 'hideMainRB');
+  looper(mainLBImg, "showImg", "hideImg");
+  looper(mainLB, "showMainLB", "hideMainLB");
+  looper(intFact, "showMainLB", "hideMainLB");
+  looper(mainRB, "showMainRB", "hideMainRB");
 
   // MENU
-  // menuResp();
+  menuResp();
 });
